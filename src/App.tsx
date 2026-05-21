8430a604d5bf150bc9d221ae4bad3951f4bb1abd
BigBrabdo <chaosx7777@gmail.com>
2026-05-20 23:51:30 -0400

    Create missing App component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>BigBoss</h1>
+        <p>Professional DJ</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
